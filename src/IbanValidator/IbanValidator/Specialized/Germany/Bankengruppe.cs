1e7ead97b719ac483ff2a1cbf64de1064c926c6e
nikeee13 <nikeee@outlook.com>
2013-11-25 17:10:20 +0100

    Chagne enum

--- src/IbanValidator/IbanValidator/Specialized/Germany/Bankengruppe.cs
+++ src/IbanValidator/IbanValidator/Specialized/Germany/Bankengruppe.cs
@@ -8,14 +8,20 @@ namespace IbanValidator.Specialized.Germany
     public enum Bankengruppe : byte
     {
         DeutscheBundesbank = 0,
-        MiscAndDeutschePostbankAg = 1,
+        DeutschePostbankAg = 1,
+        Misc1 = 1,
+        DeutschePostbankAgOrMisc1 = 1,
         Misc2 = 2,
         Misc3 = 3,
         Commerzbank1 = 4,
-        SparkasseOrLandesbank = 5,
-        GenossenschaftlicheZentralbankAndRaiffeisenbank = 6,
+        Sparkasse = 5,
+        Landesbank = 5,
+        SparkasseOrLandesbank = Sparkasse | Landesbank,
+        GenossenschaftlicheZentralbank = 6,
+        Raiffeisenbank = 6,
+        GenossenschaftlicheZentralbankOrRaiffeisenbank = GenossenschaftlicheZentralbank | Raiffeisenbank,
         DeutscheBank = 7,
         Commerzbank2 = 8,
-        Volksbanken = 9
+        Volksbank = 9
     }
 }
