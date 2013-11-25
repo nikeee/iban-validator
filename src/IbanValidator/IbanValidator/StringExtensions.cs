d5d83e615a8c3306d7c092e2dc14ab80fd2590c9
nikeee13 <nikeee@outlook.com>
2013-11-25 15:30:35 +0100

    Add string extensions

---
+++ src/IbanValidator/IbanValidator/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IbanValidator
+{
+    internal static class StringExtensions
+    {
+        public static string StripWhiteSpace(this string text)
+        {
+            return Regex.Replace(text, @"\s+", "");
+        }
+    }
+}
