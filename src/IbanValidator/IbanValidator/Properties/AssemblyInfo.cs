8be9145823ca12132f9ca5e3456a7f9c8532db05
nikeee13 <nikeee@outlook.com>
2013-11-30 00:21:46 +0100

    Update Build, AssemlbyInfo

--- src/IbanValidator/IbanValidator/Properties/AssemblyInfo.cs
+++ src/IbanValidator/IbanValidator/Properties/AssemblyInfo.cs
@@ -1,24 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("IbanValidator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("I small library used for iban and bban validation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Niklas Mollenhauer")]
 [assembly: AssemblyProduct("IbanValidator")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2013")]
+[assembly: AssemblyCopyright("Copyright © Niklas Mollenhauer 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("62913731-e2a7-4ed2-a1ae-69bbc3c9d10f")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -30,6 +22,5 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
