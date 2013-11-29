a4917280bc668d04db8e0cb9052d976ccbc936ad
nikeee13 <nikeee@outlook.com>
2013-11-30 00:24:44 +0100

    Remove comments from AssemlbyInfo

--- src/IbanValidator/IbanValidator/Properties/AssemblyInfo.cs
+++ src/IbanValidator/IbanValidator/Properties/AssemblyInfo.cs
@@ -13,14 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("62913731-e2a7-4ed2-a1ae-69bbc3c9d10f")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.*")]
